e0673f0a3b774a6c83e9426adbd8d2e6209630d0
Daniyar Orazov <orazov.video@gmail.com>
2024-01-03 23:43:07 +0100

    Additional files for creating clients

---
+++ MyModalForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿using System.ComponentModel;
+
+namespace CRM_Semester_work
+{
+    partial class MyModalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nameCompany = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.emailCompany = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameContactPerson = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.phoneCompany = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(118)))), ((int)(((byte)(120)))), ((int)(((byte)(237)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(30, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(30, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // nameCompany
+            // 
+            this.nameCompany.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameCompany.Location = new System.Drawing.Point(30, 31);
+            this.nameCompany.Margin = new System.Windows.Forms.Padding(0);
+            this.nameCompany.Name = "nameCompany";
+            this.nameCompany.Size = new System.Drawing.Size(204, 34);
+            this.nameCompany.TabIndex = 2;
+            this.nameCompany.Text = "Name company";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(30, 109);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Email";
+            // 
+            // emailCompany
+            // 
+            this.emailCompany.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.emailCompany.Location = new System.Drawing.Point(30, 146);
+            this.emailCompany.Name = "emailCompany";
+            this.emailCompany.Size = new System.Drawing.Size(228, 30);
+            this.emailCompany.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(30, 187);
+            this.label2.Margin = new System.Windows.Forms.Padding(0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 34);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Contact Person Name Surname";
+            // 
+            // nameContactPerson
+            // 
+            this.nameContactPerson.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nameContactPerson.Location = new System.Drawing.Point(30, 224);
+            this.nameContactPerson.Name = "nameContactPerson";
+            this.nameContactPerson.Size = new System.Drawing.Size(228, 30);
+            this.nameContactPerson.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(30, 267);
+            this.label3.Margin = new System.Windows.Forms.Padding(0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 34);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Phone";
+            // 
+            // phoneCompany
+            // 
+            this.phoneCompany.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.phoneCompany.Location = new System.Drawing.Point(30, 304);
+            this.phoneCompany.Name = "phoneCompany";
+            this.phoneCompany.Size = new System.Drawing.Size(228, 30);
+            this.phoneCompany.TabIndex = 7;
+            // 
+            // MyModalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.phoneCompany);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameContactPerson);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.emailCompany);
+            this.Controls.Add(this.nameCompany);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "MyModalForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MyModalForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Label nameCompany;
+
+        private System.Windows.Forms.TextBox emailCompany;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameContactPerson;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox phoneCompany;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
---
+++ MyModalForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using OfficeOpenXml;
+
+namespace CRM_Semester_work
+{
+    public partial class MyModalForm : Form
+    {
+        public MyModalForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string company = nameCompany.Text; 
+            string email = emailCompany.Text;
+            string name = nameContactPerson.Text;
+            string phone = phoneCompany.Text;
+            
+            if (company == "" || email == "" || name == "" || phone == "")
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            } 
+            
+            
+            
+            // Получаем имя файла
+            string fileName = "db.xlsx";
+
+            // Добавляем данные в файл Excel
+            AddDataToExcel(fileName, company, email, name, phone);
+
+            // Закрываем модальное окно с результатом OK
+            DialogResult = DialogResult.OK;
+        }
+        
+        private void AddDataToExcel(string fileName, string company, string email, string name, string phone)
+        {
+            // Проверяем существование файла
+            if (!File.Exists(fileName))
+            {
+                MessageBox.Show("Ошибка: Файл базы данных не найден.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            FileInfo excelFile = new FileInfo(fileName);
+
+            using (ExcelPackage excelPackage = new ExcelPackage(excelFile))
+            {
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets["Clients"];
+
+                // Определяем следующую строку для добавления данных
+                int nextRow = worksheet.Dimension?.End.Row + 1 ?? 2;
+
+                // Пример добавления данных
+                AddRowToWorksheet(worksheet, nextRow, nextRow - 1, company, email, name, phone, DateTime.Now.ToShortDateString(), DateTime.Now.ToShortDateString());
+
+                // Сохраняем изменения в файле Excel
+                excelPackage.Save();
+            }
+        }
+
+        private void AddRowToWorksheet(ExcelWorksheet worksheet, int row, params object[] values)
+        {
+            for (int col = 0; col < values.Length; col++)
+            {
+                worksheet.Cells[row, col + 1].Value = values[col];
+            }
+        }
+        
+        
+      
+    }
+
+}
